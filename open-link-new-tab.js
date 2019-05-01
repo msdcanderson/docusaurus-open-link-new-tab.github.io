4b07413800f9ab1c2d0ebac7d18191810025168e
msdcanderson <michael.anderson@banksearch-consultancy.com>
2019-05-01 15:14:48 +0100

    Update open-link-new-tab.js

--- open-link-new-tab.js
+++ open-link-new-tab.js
@@ -1,9 +1,9 @@
 /* [Source]: https://yihui.name/en/2018/09/target-blank/ */
-function() {
+(function() {
     var links = document.getElementsByTagName('a');
     for (var i = 0; i < links.length; i++) {
       if (/^(https?:)?\/\//.test(links[i].getAttribute('href'))) {
         links[i].target = '_blank';
       }
     }
-  }();
+  })();
